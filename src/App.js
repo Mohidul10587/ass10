3f09829efe23856f1420f1abb110a7a3cd323e61
mohid <mohid10587@gmail.com>
2022-08-26 17:31:08 +0600

    Remove google authintiction

--- src/App.js
+++ src/App.js
@@ -5,37 +5,20 @@ import Footer from './components/Footer';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import Blog from './components/Blog';
-
 import { Routes, Route } from "react-router-dom";
-import { auth } from './firebase.init';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { useState } from 'react';
 import LogIn from './components/LogIn';
 import ProceedCheckout from './components/ProceedCheckout';
 
-const provider = new GoogleAuthProvider()
+
 
 
 
 function App() {
-const [user , setUser] = useState()
-  const handle =()=>{
-    signInWithPopup(auth,provider)
-    .then(result=>{
-      const user = result.user ;
-      console.log(user)
-      setUser(user)
-      
-    })
-    .catch(error=>console.log(error))
-  }
-
+  
   return (
     <div className="App">
       <Header></Header>
 
-<button onClick={handle}>sing up</button>
-<p>{user?.displayName}</p>
 
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
